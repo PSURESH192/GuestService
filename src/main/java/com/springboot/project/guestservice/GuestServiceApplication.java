144fcc7ba5e4ffb96ae401c4d8652ee763d6a812
Suresh_Prabha <suresh.uday92@gmail.com>
2021-06-16 12:07:03 +0530

    Feign Client Implementation

--- src/main/java/com/springboot/project/guestservice/GuestServiceApplication.java
+++ src/main/java/com/springboot/project/guestservice/GuestServiceApplication.java
@@ -2,6 +2,8 @@ package com.springboot.project.guestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,6 +14,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableFeignClients
+@EnableDiscoveryClient
 @ComponentScan(basePackages ="com.springboot.project.guestservice")
 public class GuestServiceApplication {
 
